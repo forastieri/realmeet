ce483f1521cb10f47a70ed8f0a9e8bf2d2190d02
fabio <fabiomunizs@hotmail.com>
2022-01-25 00:31:15 -0300

    teste

---
+++ service/src/main/java/br/com/sw2you/realmeet/Teste.java
@@ -0,0 +1,7 @@
+package br.com.sw2you.realmeet;
+
+//import org.springframework.web.bind.annotation.RestController;
+//
+////@RestController
+////public class Teste implements br.com.sw2you.realmeet.api.facade.RoomsApi {
+////}
