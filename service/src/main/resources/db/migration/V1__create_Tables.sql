3a2a1503e5abe288cbc431a81b0f5e43dd60f2a4
fabio <fabiomunizs@hotmail.com>
2022-01-30 00:25:11 -0300

    criando script para criacao da tabela TB_ROOM

---
+++ service/src/main/resources/db/migration/V1__create_Tables.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE realmeet;
+
+CREATE TABLE realmeet.tb_room (
+  id_room BIGINT AUTO_INCREMENT NOT NULL,
+   name VARCHAR(255) NOT NULL,
+   nr_seats INT NOT NULL,
+   bl_active BIT(1) NULL,
+   CONSTRAINT pk_tb_room PRIMARY KEY (id_room)
+);
